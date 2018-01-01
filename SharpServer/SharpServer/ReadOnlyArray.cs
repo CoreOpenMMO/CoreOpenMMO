db238c4e32d97a0419e918c4a17ff6ab60a94346
Mirandatz <hardly.a.challenge@gmail.com>
2018-01-01 21:03:50 -0200

    Created new class with extension methods for System.Array. Created new class ReadOnlyArray to represent... Read only arrays. Further implemented SerializationManager.

---
+++ SharpServer/SharpServer/ReadOnlyArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SharpServer {
+
+    public sealed class ReadOnlyArray<T> : IReadOnlyList<T> {
+        private readonly T[] _items;
+
+        private ReadOnlyArray(T[] items) {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+
+            this._items = items;
+        }
+
+        public T this[int index] => _items[index];
+
+        public int Count => _items.Length;
+
+        public IEnumerator<T> GetEnumerator() {
+            return _items.GetGenericEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return _items.GetEnumerator();
+        }
+
+        public static ReadOnlyArray<T> WrapCollection(T[] items) {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+
+            var readOnlyArray = new ReadOnlyArray<T>(items);
+            return readOnlyArray;
+        }
+    }
+}
--- SharpServer/SharpServer/SerializationManager.cs
+++ SharpServer/SharpServer/SerializationManager.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.IO;
+using System.Text;
 
 namespace SharpServer {
 
     public sealed class SerializationManager {
+        public const string VocationsFilename = "Vocations.Json";
+
         public readonly string DataDirectoryAbsolutePath;
+
+        public string VocationsFileFullPath => Path.Combine(DataDirectoryAbsolutePath, VocationsFileFullPath);
 
         public SerializationManager(string dataDirectoryPath) {
             if (dataDirectoryPath == null)
@@ -17,6 +23,29 @@ namespace SharpServer {
             }
 
             DataDirectoryAbsolutePath = dataDirectoryAbsolutePath;
+        }
+
+        public ReadOnlyArray<Vocation> LoadVocations() {
+            if (!File.Exists(VocationsFileFullPath))
+                throw new FileNotFoundException(VocationsFileFullPath);
+
+            var serializedVocations = File.ReadAllText(VocationsFileFullPath);
+            var deserializedVocatiosn = JsonConvert.DeserializeObject<ReadOnlyArray<Vocation>>(serializedVocations);
+
+            return deserializedVocatiosn;
+        }
+
+        public void SerializeVocations(ReadOnlyArray<Vocation> vocations) {
+            if (vocations == null)
+                throw new ArgumentNullException(nameof(vocations));
+
+            var serializedVocations = JsonConvert.SerializeObject(
+                value: vocations,
+                formatting: Formatting.Indented);
+            File.WriteAllText(
+                path: VocationsFileFullPath,
+                contents: serializedVocations,
+                encoding: Encoding.UTF8);
         }
     }
 }
