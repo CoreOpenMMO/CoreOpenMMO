ac4954f14652ada03c8d3109765e50666e2f2a02
Mirandatz <hardly.a.challenge@gmail.com>
2018-01-05 00:03:27 -0200

    Renamed LoginInformation to LoginRequest. Added new fields and serialization attributes to LoginRequest. Added new methods to NetworkMessage.

---
+++ CoreOpenTibiaServer/src/CoreOpenTibiaServer.Infra.CrossCutting.Network/LoginRequest.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace COTS.Infra.CrossCutting.Network {
+
+    [JsonObject(MemberSerialization.OptOut)]
+    public sealed class LoginRequest {
+
+        [JsonProperty(Order = 0)]
+        public readonly int ClientVersion;
+
+        [JsonProperty(Order = 1)]
+        public readonly int ProtocolVersion;
+
+        [JsonProperty(Order = 2)]
+        public readonly string Account;
+
+        [JsonProperty(Order = 3)]
+        public readonly string Password;
+
+        [JsonConstructor]
+        public LoginRequest(
+            int clientVersion,
+            int protocolVersion,
+            string account,
+            string password
+            ) {
+            if (account == null)
+                throw new ArgumentNullException(nameof(account));
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+
+            ClientVersion = clientVersion;
+            ProtocolVersion = protocolVersion;
+            Account = account;
+            Password = password;
+        }
+    }
+}
--- CoreOpenTibiaServer/src/CoreOpenTibiaServer.Infra.CrossCutting.Network/NetworkMessage.cs
+++ CoreOpenTibiaServer/src/CoreOpenTibiaServer.Infra.CrossCutting.Network/NetworkMessage.cs
@@ -5,38 +5,63 @@ using System.Text;
 namespace COTS.Infra.CrossCutting.Network {
 
     public sealed class NetworkMessage {
-        public static readonly Encoding TextEncoding = Encoding.UTF8;
-        public static readonly string ContentSeparator = Environment.NewLine;
+        public static readonly Encoding TextEncoder = Encoding.UTF8;
 
         public static string Decode(byte[] bytes) {
             if (bytes == null)
                 throw new ArgumentNullException(nameof(bytes));
 
-            return TextEncoding.GetString(bytes);
+            return TextEncoder.GetString(bytes);
         }
 
         public static byte[] Encode(string message) {
             if (message == null)
                 throw new ArgumentNullException(nameof(message));
 
-            return TextEncoding.GetBytes(message);
+            return TextEncoder.GetBytes(message);
         }
 
-        public static byte[] Encode(LoginInformation loginInformation) {
+        public static byte[] EncodeAndPrependByteCount(string message) {
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+
+            var encodedMessageByteCount = TextEncoder.GetByteCount(message);
+            // +4 because a int, used to store the message length, is guaranteed to be 4 bytes
+            var buffer = new byte[4 + encodedMessageByteCount];
+
+            // Copying the encodedMessageByteCount
+            var encodedLength = BitConverter.GetBytes(encodedMessageByteCount);
+            Array.Copy(encodedLength, buffer, 4);
+
+            // Encoding the message
+            var bytesWritten = TextEncoder.GetBytes(
+               s: message,
+               charIndex: 0,
+               charCount: message.Length,
+               bytes: buffer,
+               byteIndex: 4);
+
+            if (bytesWritten != encodedMessageByteCount)
+                throw new InvalidOperationException("The Encoder lied to us >:(");
+
+            return buffer;
+        }
+
+        public static byte[] EncodeAndPrependByteCount(LoginRequest loginInformation) {
             if (loginInformation == null)
                 throw new ArgumentNullException(nameof(loginInformation));
 
             var serialized = JsonConvert.SerializeObject(loginInformation);
-            var encoded = NetworkMessage.Encode(serialized);
+            var encoded = NetworkMessage.EncodeAndPrependByteCount(serialized);
             return encoded;
         }
 
-        public static LoginInformation DecodeLoginInformation(byte[] bytes) {
+        public static LoginRequest DecodeLoginInformation(byte[] bytes) {
             if (bytes == null)
                 throw new ArgumentNullException(nameof(bytes));
 
             var decoded = NetworkMessage.Decode(bytes);
-            var deserialized = JsonConvert.DeserializeObject<LoginInformation>(decoded);
+            var deserialized = JsonConvert.DeserializeObject<LoginRequest>(decoded);
             return deserialized;
         }
     }
