9c075290140060a42edd020a8e5dccad353b7862
Mirandatz <hardly.a.challenge@gmail.com>
2018-01-28 18:51:39 -0200

    Added rules for type definitions

--- CoreOpenTibiaServer/src/COTS.GameServer/EditorConfigTest.cs
+++ CoreOpenTibiaServer/src/COTS.GameServer/EditorConfigTest.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace COTS.GameServer
-{
-    public sealed class EditorConfigTest
-    {
-		public void Method(int param) {
+namespace COTS.GameServer {
+	public sealed class EditorConfigTest {
+		private struct PrivateStruct {
 
 		}
-    }
+
+		public void Method(int param) {
+
+
+		}
+	}
 }
