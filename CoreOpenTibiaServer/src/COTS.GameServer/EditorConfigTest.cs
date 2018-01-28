3fe4b3767476985486b00efb58e8c779057fcf5d
Mirandatz <hardly.a.challenge@gmail.com>
2018-01-28 18:43:58 -0200

    Created class to test editorconfig

---
+++ CoreOpenTibiaServer/src/COTS.GameServer/EditorConfigTest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COTS.GameServer
+{
+    public sealed class EditorConfigTest
+    {
+		public void Method(int param) {
+
+		}
+    }
+}
