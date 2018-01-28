7b073a3ba51a5250b540514987275a35d751a4ee
Mirandatz <hardly.a.challenge@gmail.com>
2018-01-28 18:11:49 -0200

    Trying to make editoconfig work.

--- CoreOpenTibiaServer/src/COTS.GameServer/OTBParsing/ParsingStream.cs
+++ CoreOpenTibiaServer/src/COTS.GameServer/OTBParsing/ParsingStream.cs
@@ -1,80 +1,80 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace COTS.GameServer.OTBParsing {
 
-    /// <remarks>
-    /// !! This is a mutable struct !!
-    /// Be careful when passing it to methods!
-    /// </remarks>
-    public struct ParsingStream {
-        public readonly ByteArrayReadStream UnderlayingStream;
-        public readonly int BeginPosition;
-        public int CurrentPosition => UnderlayingStream.Position;
-        public readonly int EndPosition;
-        
-        private byte[] _parsingBuffer;
+	/// <remarks>
+	/// !! This is a mutable struct !!
+	/// Be careful when passing it to methods!
+	/// </remarks>
+	public struct ParsingStream {
+		public readonly ByteArrayReadStream UnderlayingStream;
+		public readonly int BeginPosition;
+		public int CurrentPosition => UnderlayingStream.Position;
+		public readonly int test;
 
-        public ParsingStream(ParsingTree tree, ParsingNode node) {
-            if (tree == null)
-                throw new ArgumentNullException(nameof(tree));
-            if (node == null)
-                throw new ArgumentNullException(nameof(node));
+		private byte[] _parsingBuffer;
 
-            this.UnderlayingStream = new ByteArrayReadStream(
-                array: tree.Data,
-                position: node.DataBegin);
+		public ParsingStream(ParsingTree tree, ParsingNode node) {
+			if (tree == null)
+				throw new ArgumentNullException(nameof(tree));
+			if (node == null)
+				throw new ArgumentNullException(nameof(node));
 
-            this.BeginPosition = node.DataBegin;
-            this.EndPosition = node.DataEnd;
+			this.UnderlayingStream = new ByteArrayReadStream(
+				array: tree.Data,
+				position: node.DataBegin);
 
-            // The buffer must be at least as big as the largest non-string
-            // object we can parse. Currently it's a uint32.
-            this._parsingBuffer = new byte[sizeof(UInt32)];
-        }
+			this.BeginPosition = node.DataBegin;
+			this.test = node.DataEnd;
 
-        public bool IsOver => UnderlayingStream.IsOver || CurrentPosition >= EndPosition - 1;
+			// The buffer must be at least as big as the largest non-string
+			// object we can parse. Currently it's a uint32.
+			this._parsingBuffer = new byte[sizeof(UInt32)];
+		}
 
-        public byte ReadByte() {
-            var value = UnderlayingStream.ReadByte();
+		public bool IsOver => UnderlayingStream.IsOver || CurrentPosition >= test - 1;
 
-            if ((MarkupByte)value != MarkupByte.Escape)
-                return value;
-            else
-                return UnderlayingStream.ReadByte();
-        }
+		public byte ReadByte() {
+			var value = UnderlayingStream.ReadByte();
 
-        public UInt16 ReadUInt16() {
-            for (int i = 0; i < sizeof(UInt16); i++)
-                _parsingBuffer[i] = this.ReadByte();
+			if ((MarkupByte)value != MarkupByte.Escape)
+				return value;
+			else
+				return UnderlayingStream.ReadByte();
+		}
 
-            return BitConverter.ToUInt16(_parsingBuffer, 0);
-        }
+		public UInt16 ReadUInt16() {
+			for (int i = 0; i < sizeof(UInt16); i++)
+				_parsingBuffer[i] = this.ReadByte();
 
-        public UInt32 ReadUInt32() {
-            for (int i = 0; i < sizeof(UInt32); i++)
-                _parsingBuffer[i] = this.ReadByte();
+			return BitConverter.ToUInt16(_parsingBuffer, 0);
+		}
 
-            return BitConverter.ToUInt32(_parsingBuffer, 0);
-        }
+		public UInt32 ReadUInt32() {
+			for (int i = 0; i < sizeof(UInt32); i++)
+				_parsingBuffer[i] = this.ReadByte();
 
-        public string ReadString() {
-            var stringLength = ReadUInt16();
+			return BitConverter.ToUInt32(_parsingBuffer, 0);
+		}
 
-            // "Resize" our buffer, iff necessary
-            if (stringLength > _parsingBuffer.Length)
-                _parsingBuffer = new byte[stringLength];
+		public string ReadString() {
+			var stringLength = ReadUInt16();
 
-            for (int i = 0; i < stringLength; i++)
-                _parsingBuffer[i] = ReadByte();
+			// "Resize" our buffer, iff necessary
+			if (stringLength > _parsingBuffer.Length)
+				_parsingBuffer = new byte[stringLength];
 
-            // When in C land, use C encoding...
-            return Encoding.ASCII.GetString(_parsingBuffer);
-        }
+			for (int i = 0; i < stringLength; i++)
+				_parsingBuffer[i] = ReadByte();
 
-        public void Skip(int byteCount = 1) {
-            for (int i = 0; i < byteCount; i++)
-                ReadByte();
-        }
-    }
+			// When in C land, use C encoding...
+			return Encoding.ASCII.GetString(_parsingBuffer);
+		}
+
+		public void Skip(int byteCount = 1) {
+			for (int i = 0; i < byteCount; i++)
+				ReadByte();
+		}
+	}
 }
