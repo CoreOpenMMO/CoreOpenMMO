b21c6e31be16b7c99fc0d6320e4ce0f87b4bb7aa
Mirandatz <hardly.a.challenge@gmail.com>
2018-02-03 12:32:21 -0200

    Refactored TownManager.

--- CoreOpenTibiaServer/src/COTS.GameServer/World/Town.cs
+++ CoreOpenTibiaServer/src/COTS.GameServer/World/Town.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace COTS.GameServer.World {
 	public sealed class Town : IEquatable<Town> {
--- CoreOpenTibiaServer/src/COTS.GameServer/World/TownManager.cs
+++ CoreOpenTibiaServer/src/COTS.GameServer/World/TownManager.cs
@@ -4,20 +4,28 @@ using System.Linq;
 
 namespace COTS.GameServer.World {
 	public sealed class TownManager {
-		private HashSet<Town> _towns = new HashSet<Town>();
+		private const int GuessedTownCount = 128;
+		private List<Town> _towns = new List<Town>(capacity: GuessedTownCount);
 
 		private TownManager() { }
 
-		public static TownManager Instance = new TownManager();
+		public static readonly TownManager Instance = new TownManager();
 
 		public void AddTown(Town town) {
 			if (town == null)
 				throw new ArgumentNullException(nameof(town));
 
-			if (_towns.Contains(town))
-				throw new ArgumentException(nameof(town) + " was added already.");
-			else
-				_towns.Add(town);
+			for (int i = 0; i < _towns.Count; i++) {
+				var currentTown = _towns[i];
+				if (currentTown.TownId == town.TownId)
+					throw new ArgumentException(nameof(town) + $" town with same {nameof(town.TownId)} already added.");
+				if (currentTown.TownName == town.TownName)
+					throw new ArgumentException(nameof(town) + $" town with same {nameof(town.TownName)} already added.");
+				if (currentTown.TemplePosition == town.TemplePosition)
+					throw new ArgumentException(nameof(town) + $" town with same {nameof(town.TemplePosition)} already added.");
+			}
+
+			_towns.Add(town);
 		}
 
 		public Town GetTownById(uint townId) {
@@ -26,6 +34,9 @@ namespace COTS.GameServer.World {
 		}
 
 		public Town GetTownByName(string townName) {
+			if (townName == null)
+				throw new ArgumentNullException(nameof(townName));
+
 			var town = _towns.FirstOrDefault(t => t.TownName == townName);
 			return town ?? throw new ArgumentOutOfRangeException($"There is no town with id {townName};");
 		}
