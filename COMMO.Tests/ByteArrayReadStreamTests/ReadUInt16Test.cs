df0be492c8b8fa2cf519de2d7664204832ef64ef
Igor Pauli Quintaes <igor.quintaes@lambda3.com.br>
2018-03-21 14:51:52 -0300

    readuint16 tests

--- COMMO.Tests/ByteArrayReadStreamTests/ReadUInt16Test.cs
+++ COMMO.Tests/ByteArrayReadStreamTests/ReadUInt16Test.cs
@@ -4,39 +4,39 @@ using System;
 using System.Linq;
 
 namespace COMMO.Tests.ByteArrayReadStreamTests {
-	public class ReadUInt32Test {
+	public class ReadUInt16Test {
 
 		[TestCase(new byte[] { 0, 7 }, 2)]
 		public void ShouldThrowExceptionIfArrayIsOver(byte[] array, int position) {
 			var objectResult = new ByteArrayReadStream(array, position);
-			Action action = () => { objectResult.ReadUInt32(); };
+			Action action = () => { objectResult.ReadUInt16(); };
 			action.Should().ThrowExactly<InvalidOperationException>();
 		}
 
-		[TestCase((uint)7)]
-		[TestCase((uint)1212)]
-		[TestCase(uint.MaxValue)]
-		[TestCase(uint.MinValue)]
-		public void ShouldReadAndSkipPositionsProperlySingleValue(uint value) {
+		[TestCase((ushort)7)]
+		[TestCase((ushort)1212)]
+		[TestCase(ushort.MaxValue)]
+		[TestCase(ushort.MinValue)]
+		public void ShouldReadAndSkipPositionsProperlySingleValue(ushort value) {
 			var bytearray = BitConverter.GetBytes(value);
 			var byteArrayReader = new ByteArrayReadStream(bytearray);
-			var result = byteArrayReader.ReadUInt32();
+			var result = byteArrayReader.ReadUInt16();
 
 			result.Should().Be(value);
-			byteArrayReader.Position.Should().Be(sizeof(uint));
+			byteArrayReader.Position.Should().Be(sizeof(ushort));
 			byteArrayReader.IsOver.Should().BeTrue();
 		}
-		[TestCase(new uint[] { 0, 7 }, false)]
-		[TestCase(new uint[] { 654, 9687, 212 }, false)]
-		[TestCase(new uint[] { uint.MaxValue, uint.MinValue, 3 }, false)]
-		[TestCase(new uint[] { 2 }, true)]
-		public void ShouldReadAndSkipPositionsProperlyArrayOfValues(uint[] values, bool isOver) {
+		[TestCase(new ushort[] { 0, 7 }, false)]
+		[TestCase(new ushort[] { 654, 9687, 212 }, false)]
+		[TestCase(new ushort[] { ushort.MaxValue, ushort.MinValue, 3 }, false)]
+		[TestCase(new ushort[] { 2 }, true)]
+		public void ShouldReadAndSkipPositionsProperlyArrayOfValues(ushort[] values, bool isOver) {
 			var bytearray = values.SelectMany(BitConverter.GetBytes).ToArray();
 			var byteArrayReader = new ByteArrayReadStream(bytearray);
-			var result = byteArrayReader.ReadUInt32();
+			var result = byteArrayReader.ReadUInt16();
 
 			result.Should().Be(values[0]);
-			byteArrayReader.Position.Should().Be(sizeof(uint));
+			byteArrayReader.Position.Should().Be(sizeof(ushort));
 			byteArrayReader.IsOver.Should().Be(isOver);
 		}
 	}
