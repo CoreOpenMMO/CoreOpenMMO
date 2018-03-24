0217c9884af59054b198aeab0e0cf994e93b69e0
Mirandatz <Mirandatz@users.noreply.github.com>
2018-03-24 14:12:35 -0300

    QuadTree leaf nodes no longer allocate memory for children nodes.

--- COMMO.GameServer/World/QuadTreeNode.cs
+++ COMMO.GameServer/World/QuadTreeNode.cs
@@ -1,13 +1,19 @@
+using System;
+
 namespace COMMO.GameServer.World {
 
 	public class QuadTreeNode {
 		private const int QuadTreeNodeChildCount = 4;
-		public readonly QuadTreeNode[] Children = new QuadTreeNode[QuadTreeNodeChildCount];
+		public readonly QuadTreeNode[] Children;
 
 		public readonly bool IsLeaf;
 
 		protected QuadTreeNode(bool isLeaf) {
 			IsLeaf = isLeaf;
+			if (IsLeaf)
+				Children = Array.Empty<QuadTreeNode>();
+			else
+				Children = new QuadTreeNode[QuadTreeNodeChildCount];
 		}
 
 		public QuadTreeNode()
