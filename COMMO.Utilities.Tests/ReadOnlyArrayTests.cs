b8623a9e015bd5f34024aeb18bb7658b02c03435
Mirandatz <Mirandatz@users.noreply.github.com>
2018-07-10 14:24:25 -0300

    Wrote basic tests for ReadOnlyArray.

--- COMMO.Utilities.Tests/ReadOnlyArrayTests.cs
+++ COMMO.Utilities.Tests/ReadOnlyArrayTests.cs
@@ -1,11 +1,34 @@
 namespace COMMO.Utilities.Tests {
+	using COMMO.Utilities;
+	using System;
 	using Xunit;
 
 	public sealed class ReadOnlyArrayTests {
 
+		/// <summary>
+		/// Checks whether the WrapCollection accepts null arguments.
+		/// It shouldn't.
+		/// </summary>
 		[Fact]
-		public void Test1() {
+		public void WrapNullReference() {
+			Assert.Throws<ArgumentNullException>(() => {
+				var readOnlyArray = ReadOnlyArray<int>.WrapCollection(null);
+			});
+		}
 
+		[Theory]
+		[InlineData(0, 0)]
+		[InlineData(0, -1)]
+		[InlineData(0, 1)]
+		[InlineData(1, -1)]
+		[InlineData(1, 1)]
+		public void AccessElementsOutOfBounds(int arrayLength, int elementIndex) {
+			var array = new int[arrayLength];
+			var wrapper = ReadOnlyArray<int>.WrapCollection(array);
+
+			Assert.ThrowsAny<IndexOutOfRangeException>(() => {
+				var values = wrapper[elementIndex];
+			});
 		}
 	}
 }
