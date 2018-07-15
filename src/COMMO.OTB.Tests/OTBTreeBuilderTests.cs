f80c4cb5dc84e4855d35311746dce1c126d45143
Mirandatz <Mirandatz@users.noreply.github.com>
2018-07-15 16:43:12 -0300

    Further implemented OTBMWorldLoader.

--- src/COMMO.OTB.Tests/OTBTreeBuilderTests.cs
+++ src/COMMO.OTB.Tests/OTBTreeBuilderTests.cs
@@ -2,6 +2,7 @@ namespace COMMO.OTB.Tests {
 	using Xunit;
 
 	public sealed class OTBTreeBuilderTests {
+
 		[Fact]
 		public void WeNeedToWriteTestsForThis() {
 			Assert.True(false);
--- src/COMMO.Server/World/OTBMWorldLoader.cs
+++ src/COMMO.Server/World/OTBMWorldLoader.cs
@@ -1,6 +1,6 @@
 namespace COMMO.Server.World {
+	using COMMO.OTB;
 	using System;
-	using System.IO;
 
 	/// <summary>
 	/// This class contains the methods necessary to load a .otbm file.
@@ -8,15 +8,60 @@ namespace COMMO.Server.World {
 	public static partial class OTBMWorldLoader {
 
 		/// <summary>
+		/// This class only support items encoded using this major version.
+		/// </summary>
+		public const uint SupportedItemEncodingMajorVersion = 3;
+
+		/// <summary>
+		/// This class only support items encoded using this minor version.
+		/// </summary>
+		public const uint SupportedItemEncodingMinorVersion = 8;
+
+		/// <summary>
 		/// Loads a .otbm file, parse it's contents and returns a <see cref="COMMO.Server.World.World"/>.
 		/// </summary>
-		public static World LoadWorld(string filename) {
-			if (filename == null)
-				throw new ArgumentNullException(nameof(filename));
+		public static World LoadWorld(ReadOnlyMemory<byte> serializedWorldData) {
 
-			var serializedWorldData = File.ReadAllBytes(filename);
+			var world = new World();
+			var otbTree = OTBDeserializer.DeserializeOTBData(serializedWorldData);
+
+			ParseOTBTreeRootNode(otbTree);
 
 			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Logs the information embedded in the root node of the OTB tree.
+		/// </summary>
+		private static void ParseOTBTreeRootNode(OTBNode rootNode) {
+			if (rootNode == null)
+				throw new ArgumentNullException(nameof(rootNode));
+			if (rootNode.Children.Count != 1)
+				throw new InvalidOperationException();
+
+			var parsingStream = new OTBParsingStream(rootNode.Data);
+
+			var headerVersion = parsingStream.ReadUInt32();
+			if (headerVersion == 0 || headerVersion > 2)
+				throw new InvalidOperationException();
+
+			var worldWidth = parsingStream.ReadUInt16();
+			var worldHeight = parsingStream.ReadUInt16();
+
+			var itemEncodingMajorVersion = parsingStream.ReadUInt32();
+			if (itemEncodingMajorVersion != SupportedItemEncodingMajorVersion)
+				throw new InvalidOperationException();
+
+			var itemEncodingMinorVersion = parsingStream.ReadUInt32();
+			if (itemEncodingMinorVersion < SupportedItemEncodingMinorVersion)
+				throw new InvalidOperationException();
+
+			// TODO: use decent loggin methods
+			Console.WriteLine($"OTBM header version: {headerVersion}");
+			Console.WriteLine($"World width: {parsingStream.ReadUInt16()}");
+			Console.WriteLine($"World height: {parsingStream.ReadUInt16()}");
+			Console.WriteLine($"Item encoding major version: {parsingStream.ReadUInt32()}");
+			Console.WriteLine($"Item encoding minor version: {parsingStream.ReadUInt32()}");
 		}
 	}
 }
