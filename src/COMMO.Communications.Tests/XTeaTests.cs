dbf9f5d642c8acf29a180e718c8ad8ff5f78b35b
Mirandatz <Mirandatz@users.noreply.github.com>
2018-08-18 15:42:28 -0300

    Wrote legacy-compatibility test for XTea.

--- src/COMMO.Communications.Tests/XTeaTests.cs
+++ src/COMMO.Communications.Tests/XTeaTests.cs
@@ -1,7 +1,8 @@
 namespace COMMO.Communications.Tests {
+	using System;
+	using System.Runtime.InteropServices;
 	using COMMO.Communications.Criptography;
 	using NUnit.Framework;
-	using System;
 
 	[TestFixture]
 	public sealed class XTeaTests {
@@ -22,6 +23,48 @@ namespace COMMO.Communications.Tests {
 					message: message,
 					key: key)
 				);
+		}
+
+
+		[Test, Combinatorial]
+		public void LegacyCompatibility_Encrypt(
+			[Values(
+			new UInt64[] { 0, 1},
+			new UInt64[] { 0, 1, 2})] UInt64[] message,
+			[Values(
+			new UInt32[] { 0, 1, 2, 3 },
+			new UInt32[] { 0, 0, 0, 0 })] UInt32[] key
+			) {
+			var messageAsBytes = MemoryMarshal.Cast<UInt64, byte>(message);
+			var keyAsBytes = MemoryMarshal.Cast<UInt32, byte>(key);
+
+			var legacyEncrypted = LegacyEncrypt(messageAsBytes, keyAsBytes);
+			var newEncrypted = XTea.Encrypt(
+				message: messageAsBytes,
+				key: keyAsBytes).ToArray();
+
+			Assert.That(newEncrypted, Is.EquivalentTo(legacyEncrypted));
+		}
+
+
+		/// <summary>
+		/// Coz legacy code encrypts in-place.
+		/// </summary>
+		private static byte[] LegacyEncrypt(Span<byte> message, Span<byte> key) {
+			var messageCopy = message.ToArray();
+			var keyCopy = key.ToArray();
+			var keyAsUInt32 = MemoryMarshal
+				.Cast<byte, UInt32>(keyCopy)
+				.ToArray();
+			var length = messageCopy.Length;
+
+			Security.Encryption.Xtea.Encrypt(
+				buffer: ref messageCopy,
+				length: ref length,
+				index: 0,
+				key: keyAsUInt32);
+
+			return messageCopy;
 		}
 	}
 }
